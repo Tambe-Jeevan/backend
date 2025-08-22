160712ec767bf618a15d06943a208cca09125420
Jeevan Tambe <tambejeevan816@gmail.com>
2025-08-22 10:35:07 +0530

    Add Todo Project Backend Example.

--- 03mangoose_models/models/todosAppBackend/sub_todo.models.js
+++ 03mangoose_models/models/todosAppBackend/sub_todo.models.js
@@ -1,10 +1,21 @@
 import mongoose from "mongoose";
 
-const subTodo = new mongoose.Schema(
-    {
-
+const subTodoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
     },
-    {timestamps:true},
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
 );
 
-export const SubTodo = mongoose.model("SubTodo", subTodo);
+export const SubTodo = mongoose.model("SubTodo", subTodoSchema);
--- 03mangoose_models/models/todosAppBackend/todo.models.js
+++ 03mangoose_models/models/todosAppBackend/todo.models.js
@@ -1,11 +1,29 @@
 import mongoose from "mongoose";
 
 const todoSchema = new mongoose.Schema(
-    {
-
+  {
+    content: {
+      type: String,
+      required: true,
     },
-    {timestamps: true}
-)
-
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    // Relation between User and Todo Model
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    // Realtion between Todos And SubTodo Model as array
+    subTodos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubTodo",
+      },
+    ], //
+  },
+  { timestamps: true }
+);
 
 export const Todo = mongoose.model("Todo", todoSchema);
